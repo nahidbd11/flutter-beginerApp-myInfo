cd882783b65a7a60a3d058b355ed74c4ad7335a3
nahidul islam <diegonahid@gmail.com>
2022-05-31 10:25:32 +0600

    from statefull-widget changes

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 // ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
 
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -8,13 +10,38 @@ void main() {
   ));
 }
 
-class MyInfoApp extends StatelessWidget {
+class MyInfoApp extends StatefulWidget {
   const MyInfoApp({Key? key}) : super(key: key);
+
+  @override
+  State<MyInfoApp> createState() => _MyInfoAppState();
+}
+
+class _MyInfoAppState extends State<MyInfoApp> {
+  final random = Random();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black87,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            random.nextInt(100); //change the state on press the floating button
+          });
+        },
+        backgroundColor: Color.fromARGB(137, 156, 131, 131),
+        child: Icon(
+          Icons.add,
+          color: Colors.amberAccent,
+        ),
+      ),
       appBar: AppBar(
+        title: const Text(
+          "Nahid's info statefull",
+          style: TextStyle(
+            letterSpacing: 2,
+          ),
+        ),
         elevation: 0,
         backgroundColor: Color.fromARGB(255, 24, 22, 22),
         centerTitle: true,
@@ -93,6 +120,14 @@ class MyInfoApp extends StatelessWidget {
                   "nahid801@gmail.com",
                   style: TextStyle(
                     color: Colors.amberAccent,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                SizedBox(width: 10),
+                Text(
+                  "(Total ${random.nextInt(100)} sms)",
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 226, 63, 14),
                     fontStyle: FontStyle.italic,
                   ),
                 ),
